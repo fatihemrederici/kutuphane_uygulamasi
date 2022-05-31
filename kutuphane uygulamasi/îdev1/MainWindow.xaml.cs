f53dcaf81eccf8d45de27baf23e2bfe234a06549
fatihemrederici <106096768+fatihemrederici@users.noreply.github.com>
2022-05-31 15:42:38 +0300

    Add files via upload

---
+++ kutuphane uygulamasi/îdev1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ödev1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBox1.Items.Add(TextBox1.Text + " " + "-" + " " + TextBox2.Text);
+        }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            int sil = ComboBox1.SelectedIndex;
+            ComboBox1.Items.RemoveAt(sil);
+        }
+    }
+}
